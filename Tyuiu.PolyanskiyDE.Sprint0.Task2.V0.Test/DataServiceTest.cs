527fb793b8601c4d69e87f98baca831a811812bc
Tyuiu.PolyanskiyDE <danilp.2005@mail.ru>
2023-09-17 19:11:50 +0500

    Task2

---
+++ Tyuiu.PolyanskiyDE.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PolyanskiyDE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PolyanskiyDE.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Данил";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Данил", res);
+        }
+    }
+}
---
+++ Tyuiu.PolyanskiyDE.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PolyanskiyDE.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PolyanskiyDE.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Данил"));
+            Console.ReadKey();
+        }
+    }
+}
